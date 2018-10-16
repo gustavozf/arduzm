9c3dc9f60d0d64f75922efb1f69f7b8f06a513c0
gustavozf <gzf1996@gmail.com>
2018-10-15 21:46:28 -0300

    new examples

--- button/button.h
+++ button/button.h
@@ -1,5 +1,9 @@
 #include <avr/io.h>
 
+typedef struct{
+    uint8_t state, pressed, i;
+} button;
+
 void createButton(uint8_t i);
 void waitButtonRelease(uint8_t i);
 uint8_t getButtonClick(uint8_t i);
